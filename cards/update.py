a394a319aa839afb6f40699696b655624ea0d986
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2022-05-13 13:55:28 -0600

    made picture section optional for proper css formatting

---
+++ cards/update.py
@@ -0,0 +1,5 @@
+# TODO create script to automate the model
+# https://github.com/Ajatt-Tools/AnkiNoteTypes/blob/main/antp/updater.py
+# https://github.com/FooSoft/anki-connect#model-actions
+# updateModelTemplates
+# updateModelStyling
