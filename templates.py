45b08b8cdbb7edd62449192a4b2ba2f38ac7d078
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2022-07-09 12:29:32 -0600

    touchups to the info circle

--- templates.py
+++ templates.py
@@ -207,6 +207,42 @@ r"""
 """,
 
 
+"info_circle":
+
+r"""
+<span class="info-circle" id="info_circle">
+  <span class="info-circle-svg-wrapper">
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="#585858" class="bi bi-info-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+    </svg>
+  </span>
+
+  <span class="info-circle-text-wrapper">
+    <span class="info-circle-text" id="info_circle_text">
+      <div class="info-circle-text-error" id="info_circle_text_error"></div>
+      <div class="info-circle-text-warning" id="info_circle_text_warning"></div>
+      <div class="info-circle-text-info" id="info_circle_text_info">
+        <div>
+          Need help? View the
+          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note/wiki">documentation</a>.
+        </div>
+        <div>
+          Have an issue?
+          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note/issues">Report it here</a>.
+        </div>
+        <div>
+          View the
+          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note">source code</a>.
+        </div>
+      </div>
+    </span>
+  </span>
+</span>
+""",
+
+
+
 "full_sentence_front":
 
 r"""
@@ -497,30 +533,7 @@ r"""
 
   {{/PADoNotShowInfoLegacy}}
 
-  <span class="info-circle" id="info_circle">
-    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="#585858" class="bi bi-info-circle" viewBox="0 0 16 16">
-      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-    </svg>
-    <span class="info-circle-text" id="info_circle_text">
-      <div class="info-circle-text-error" id="info_circle_text_error"></div>
-      <div class="info-circle-text-warning" id="info_circle_text_warning"></div>
-      <div class="info-circle-text-info" id="info_circle_text_info">
-        <div>
-          Need help? View the
-          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note/wiki">documentation</a>.
-        </div>
-        <div>
-          Have an issue?
-          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note/issues">Report it here</a>.
-        </div>
-        <div>
-          View the
-          <a href="https://github.com/Aquafina-water-bottle/jp-mining-note">source code</a>.
-        </div>
-      </div>
-    </span>
-  </span>
+  {{INFO_CIRCLE}}
 
   {{VERSION}}
 </div>
@@ -888,6 +901,9 @@ r"""
 
 <div class="card-description">
   PA Sentence
+
+  {{INFO_CIRCLE}}
+
   {{VERSION}}
 </div>
 
@@ -951,6 +967,9 @@ r"""
 
 <div class="card-description">
   PA Word
+
+  {{INFO_CIRCLE}}
+
   {{VERSION}}
 </div>
 
@@ -986,6 +1005,9 @@ r"""
 
 <div class="card-description">
   Cloze Deletion
+
+  {{INFO_CIRCLE}}
+
   {{VERSION}}
 </div>
 
