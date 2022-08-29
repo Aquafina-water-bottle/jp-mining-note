44ce23840ef484656e7d5e88d5a715abd765075c
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2022-08-28 19:36:28 -0600

    added option to auto color pa-sent card quotes, cleaned up js code a bit by removing most  declarations

--- templates/jp-mining-note/main/common.js
+++ templates/jp-mining-note/main/common.js
@@ -1,60 +1,64 @@
 /// {% set functions %}
-  function hybridClick() {
-    var hSent = document.getElementById("hybrid-sentence");
-    var hWord = document.getElementById("hybrid-word");
-    var svgEle = document.getElementById("flag_box_svg");
-    var circ = document.getElementById("svg_circle");
+function hybridClick() {
+  let hSent = document.getElementById("hybrid-sentence");
+  let hWord = document.getElementById("hybrid-word");
+  let svgEle = document.getElementById("flag_box_svg");
+  let circ = document.getElementById("svg_circle");
 
 
-    if (hSent.classList.contains("override-display-inline-block")) {
-      // currently showing sentence, change to word
-      hWord.classList.remove("override-display-none");
-      hSent.classList.remove("override-display-inline-block");
-      if (circ !== null) {
-        circ.setAttributeNS(null, "cx", "25");
-        circ.setAttributeNS(null, "cy", "15");
-      }
-      // re-adds if quote module enabled
-      if (svgEle !== null && note.colorQuotes) {
-        svgEle.style.display = "initial";
-      }
+  if (hSent.classList.contains("override-display-inline-block")) {
+    // currently showing sentence, change to word
+    hWord.classList.remove("override-display-none");
+    hSent.classList.remove("override-display-inline-block");
+    if (circ !== null) {
+      circ.setAttributeNS(null, "cx", "25");
+      circ.setAttributeNS(null, "cy", "15");
+    }
+    // re-adds if quote module enabled
+    if (svgEle !== null && note.colorQuotes) {
+      svgEle.style.display = "initial";
+    }
 
-    } else {
-      // currently showing word, change to sentence
-      hWord.classList.add("override-display-none");
-      hSent.classList.add("override-display-inline-block");
-      if (circ !== null) { // sentence
-        if (hSent.innerText.length > 0 && hSent.innerText[0] === "「") {
-          circ.setAttributeNS(null, "cx", "35");
-          circ.setAttributeNS(null, "cy", "11");
-        }
-      }
-      // removes if quote module enabled
-      if (svgEle !== null && note.colorQuotes) {
-        svgEle.style.display = "none";
+  } else {
+    // currently showing word, change to sentence
+    hWord.classList.add("override-display-none");
+    hSent.classList.add("override-display-inline-block");
+    if (circ !== null) { // sentence
+      if (hSent.innerText.length > 0 && hSent.innerText[0] === "「") {
+        circ.setAttributeNS(null, "cx", "35");
+        circ.setAttributeNS(null, "cy", "11");
       }
     }
+    // removes if quote module enabled
+    if (svgEle !== null && note.colorQuotes) {
+      svgEle.style.display = "none";
+    }
   }
+}
 
 /// {% endset %}
 
 
 /// {% set keybind_settings %}
 
-  //keys = settings.keybind("toggle-hybrid-sentence");
-  keys = {{ utils.opt("keybinds", "toggle-hybrid-sentence") }};
-  var hSent = document.getElementById("hybrid-sentence");
-  var hWord = document.getElementById("hybrid-word");
-  if (keys !== null && hSent && hWord && keys.includes(e.key)) {
+//keys = settings.keybind("toggle-hybrid-sentence");
+keys = {{ utils.opt("keybinds", "toggle-hybrid-sentence") }};
+if (keys !== null && keys.includes(e.key)) {
+  let hSent = document.getElementById("hybrid-sentence");
+  let hWord = document.getElementById("hybrid-word");
+  if (hSent !== null && hWord !== null) {
     hybridClick();
   }
+}
 
-  //keys = settings.keybind("toggle-highlight-word");
-  keys = {{ utils.opt("keybinds", "toggle-highlight-word") }};
-  var paButton = document.getElementById("pa-button");
-  if (keys !== null && paButton && keys.includes(e.key)) {
+//keys = settings.keybind("toggle-highlight-word");
+keys = {{ utils.opt("keybinds", "toggle-highlight-word") }};
+if (keys !== null && keys.includes(e.key)) {
+  let paButton = document.getElementById("pa-button");
+  if (paButton !== null) {
     toggleHighlightWord();
   }
+}
 
 /// {% endset %}
 
@@ -62,11 +66,15 @@
 
 
 /// {% set run %}
-  var isAltDisplay = !!'{{ utils.any_of_str("AltDisplay") }}';
+{
+  let isAltDisplay = !!'{{ utils.any_of_str("AltDisplay") }}';
   processSentences(isAltDisplay);
 
+  let d = document.getElementById("Display");
+  let circ = document.getElementById("svg_circle");
+  let svgTitle = document.getElementById("svg_title");
+
   /// {% call IF("IsClickCard") %}
-  var d = document.getElementById("Display");
   d.onclick = hybridClick;
   /// {% endcall %}
 
@@ -77,8 +85,6 @@
   // More specifically, checks if the first character is "「",
   // and adjusts the position based on that
 
-  var circ = document.getElementById("svg_circle");
-  var d = document.getElementById("Display");
   if ('{{ utils.any_of_str("IsSentenceCard", "IsTargetedSentenceCard") }}' &&
       d.innerText.length > 0 && d.innerText[0] == "「") { // TODO: compatability with quote module
     circ.setAttributeNS(null, "cx", "35");
@@ -89,16 +95,13 @@
   //  Word pitch indicator color
   // ============================
   // done in javascript to simplify templating logic
-  var svgTitle = document.getElementById("svg_title");
   if (svgTitle !== null) {
     svgTitle.textContent = "PA: " + paIndicator.tooltip;
   }
 
-  var circ = document.getElementById("svg_circle");
   circ.classList.add(paIndicator.className);
   /// {% endcall %}
-
-
+}
 /// {% endset %}
 
 
