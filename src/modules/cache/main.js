97ae4b845de660a1f4726fc31643926eb3b96d06
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2022-11-28 09:23:54 -0700

    removed generate svg functions by moving the svgs into the hidden div

--- src/modules/cache/main.js
+++ src/modules/cache/main.js
@@ -23,11 +23,9 @@ const JPMNCache = (() => {
   const logger = new JPMNLogger("cache");
 
   function generateClearCacheSVG() {
-    const svgStr = '<svg id="settings_clear_cache" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9M7 6h10v13H7V6m2 2v9h2V8H9m4 0v9h2V8h-2Z"/><title>Clear the cache</title></svg>';
-
-    const x = document.createElement("span");
-    x.innerHTML = svgStr;
-    return x.children[0];
+    const x = document.getElementById("hidden_svg_settings_clear_cache")
+    let result = x.children[0].cloneNode(true);
+    return result;
   }
 
 
