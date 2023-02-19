9b0d1af79b09e3225a91ac2bb487d0dab8e80208
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-02-19 15:52:07 -0700

    added tags to info circle

--- src/ts/modules/infoCircleUtils.ts
+++ src/ts/modules/infoCircleUtils.ts
@@ -1,6 +1,7 @@
+import {invoke} from '../ankiConnectUtils';
 import { RunnableModule } from '../module';
 import { O, getOption } from '../options';
-import { isMobile, popupMenuMessage } from '../utils';
+import { getCardSide, isMobile, popupMenuMessage, TAGS_LIST } from '../utils';
 
 const infoCircFrozen = 'info-circle--frozen';
 const infoCircTogglable = 'info-circle-svg-wrapper--togglable';
@@ -10,6 +11,7 @@ const infoCircHoverColor = 'info-circle--hover-color';
 export class InfoCircleUtils extends RunnableModule {
   private readonly infoCirc = document.getElementById('info_circle');
   private readonly infoCircWrapper = document.getElementById('info_circle_wrapper');
+  private readonly infoCircTags = document.getElementById('info_circle_text_tags');
 
   constructor() {
     super("infoCircleUtils");
@@ -19,6 +21,20 @@ export class InfoCircleUtils extends RunnableModule {
     const isHoverable = getOption('infoCircleUtils.isHoverable');
     const togglableLockshowPopup = getOption('infoCircleUtils.togglableLock.showPopup');
     const togglableLockEnabled = getOption('infoCircleUtils.togglableLock.enabled');
+
+    if (this.infoCircTags !== null) {
+      const showTagsMode = getOption("infoCircleUtils.showTagsMode");
+      if ((showTagsMode === "always") || (showTagsMode === "back" && getCardSide() === "back")) {
+        for (const tag of TAGS_LIST) {
+          const tagEle = document.createElement("span")
+          tagEle.innerText = tag;
+          tagEle.onclick = () => {
+            invoke('guiBrowse', { query: `tag:${tag}` });
+          }
+          this.infoCircTags.appendChild(tagEle);
+        }
+      }
+    }
 
     if (this.infoCircWrapper === null || this.infoCirc === null) {
       return;
