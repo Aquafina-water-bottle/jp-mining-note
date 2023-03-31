7b6fd7d5886c632e61c5961f605d2910cb4199c7
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-03-30 20:01:11 -0600

    updated pip requirements and mkdocs stuffs, WIP builder

---
+++ tools/json_handler.py
@@ -0,0 +1,11 @@
+
+class JsonHandler:
+    def __init__(self):
+        pass
+
+    def read_file(self, file_name: str):
+        with open(file_name) as f:
+            pass
+
+    def read_string(self):
+        pass
