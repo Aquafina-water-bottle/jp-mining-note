d671a93ccf8c2bbd349e653f79e6a654423bda66
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-04-01 01:51:48 -0600

    WIP cache.ts now runs properly with node

--- src/ts/jp-mining-note/cache.ts
+++ src/ts/jp-mining-note/cache.ts
@@ -1,37 +1,64 @@
+import { JSDOM } from 'jsdom';
+//import { xhr2 } from 'xhr2';
 import {KanjiHover} from '../modules/kanjiHover';
 import {invoke} from '../ankiConnectUtils';
+import {NoteInfo} from '../utils';
+import {Field} from '../fields';
+
+
+
+function simulateEnv(noteInfo: NoteInfo) {
+  const h = document.getElementById("hidden") as HTMLElement;
+  h.innerHTML = "";
+
+  for (const key of Object.keys(noteInfo.fields)) {
+
+    const val = noteInfo.fields[key as Field].value;
+
+    const eleVal = document.createElement("div");
+    eleVal.setAttribute("id", `hidden_field_${key}`);
+    eleVal.innerHTML = val;
+
+    const eleExists = document.createElement("div");
+    eleExists.setAttribute("id", `hidden_field_exists_${key}`);
+    eleExists.innerHTML = val.length > 0 ? "1" : "";
+
+    h.appendChild(eleVal);
+    h.appendChild(eleExists);
+  }
+
+}
 
 async function calcKanjiHover() {
 
-  const kanjiHover = new KanjiHover();
-  kanjiHover.main();
+  //globalThis.XMLHttpRequest = xhr2;
+  globalThis.XMLHttpRequest = require('xhr2');
 
   // TODO cli flags
-  const query = `"note:JP Mining Note" due:1`;
+  const query = `"note:JP Mining Note" prop:due=0`;
   console.log("Querying due notes...")
   const notes = await invoke("findNotes", { query: query }) as number[];
 
   console.log("Getting notes info...")
-  const notesInfo = await invoke("notesInfo", { notes: notes }) as any[];
-  console.log(notesInfo)
+  const notesInfo = await invoke("notesInfo", { notes: notes }) as NoteInfo[];
 
   for (const info of notesInfo ) {
     console.log("Key:", info.fields.Key.value);
+    simulateEnv(info);
+
+    const kanjiHover = new KanjiHover();
+    kanjiHover.main();
     break;
   }
 
 }
 
 function main() {
-  const calcKanjiHoverBtn = document.getElementById("calc_kanji_hover")
-  if (calcKanjiHoverBtn) {
-    calcKanjiHoverBtn.onclick = (() => {
-      console.log("A");
-      calcKanjiHover();
-    });
-  } else {
-    console.log("B");
-  }
+  //console.log("Hello world!");
+  const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p><div id="hidden"></div>`);
+  globalThis.document = dom.window.document;
+
+  calcKanjiHover();
 }
 
 main();
