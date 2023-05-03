53f0e0dad58ec3ec55b659915e151ae0b357f885
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-05-03 17:04:00 -0600

    fix: tightened the conditions for sentence reading equality check

--- src/ts/modules/sentenceParser.ts
+++ src/ts/modules/sentenceParser.ts
@@ -505,7 +505,7 @@ export class SentenceParser extends RunnableModule {
   private compareSentenceReading() {
     const sentence = getFieldValue("Sentence")
     const sentReading = plainToKanjiOnly(getFieldValue("SentenceReading"));
-    if (sentence !== sentReading) {
+    if (sentReading.trim().length !== 0 && sentence.trim() !== sentReading.trim()) {
       this.logger.warn(`The Sentence field is not the same as the SentenceReading field. Your sentence might be displayed incorrectly. See <a href="https://aquafina-water-bottle.github.io/jp-mining-note-prerelease/faq/#the-sentencereading-field-is-not-updated-is-different-from-the-sentence-field">here</a> for more info.`, {isHtml: true})
     }
   }
