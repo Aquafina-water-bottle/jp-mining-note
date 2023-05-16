4f6643e6c5c21f35036b44bdb16cf5242c70d582
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-05-15 21:33:56 -0600

    feat: textbender integration with bolded definitions

--- src/ts/modules/mobileUtils.ts
+++ src/ts/modules/mobileUtils.ts
@@ -1,5 +1,5 @@
 import { RunnableModule } from '../module';
-import { isMobile } from '../utils';
+import { isAndroid, isMobile } from '../utils';
 import { fieldsAnyFilled } from '../fields';
 
 export class MobileUtils extends RunnableModule {
@@ -17,6 +17,20 @@ export class MobileUtils extends RunnableModule {
       document
         .getElementById('display')
         ?.classList.toggle('expression__hybrid--click-hover-effect', false);
+    }
+
+    if (isAndroid()) {
+      const textBenderDef = document.querySelector(`.glossary__external-links a[data-details="yomichan-textbender-definition"]`) as HTMLAnchorElement | null;
+      if (textBenderDef) {
+        // grabs all bolded text in the definition, if it exists
+        let boldText = ""
+        for (const boldTextEle of document.querySelectorAll(`#primary_definition_raw_text b`)) {
+          boldText += boldTextEle.textContent;
+        }
+        if (boldText.length > 0) { // bold text was actually found
+          textBenderDef.href = `textbender://x?x=${boldText}`;
+        }
+      }
     }
   }
 }
