0ecf9b373b64d8ac747e2bcf161a6deac4777d52
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-05-29 14:32:45 -0600

    refactor: removed quotes in json5, skip checking existence of comment

--- tools/fields.py
+++ tools/fields.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import os
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Any
 from json_handler import JsonHandler
 
 import utils
@@ -40,16 +40,32 @@ class Field:
     # whether it is collapsed by default or not
     default_collapsed: Optional[bool] = None
 
+    # whether it is used in the "hidden_field_exists_*" divs in the HTML
+    ignore_field_exists: Optional[bool] = None
 
-def get_fields(json_handler: JsonHandler):
+
+def get_fields(json_handler: JsonHandler) -> list[Field]:
     fields = []
-    file_path = os.path.join(utils.get_root_folder(), "data", "fields.json5")
-    for json_data in json_handler.read_file(file_path)["fields"]:
+    for json_data in get_fields_json(json_handler)["fields"]:
         field = Field(**json_data)
         fields.append(field)
-
     return fields
 
+
+def get_fields_json(json_handler: JsonHandler) -> dict[str, Any]:
+    file_path = os.path.join(utils.get_root_folder(), "data", "fields.json5")
+    return json_handler.read_file(file_path)
+
+
+def get_fields_map(json_handler: JsonHandler) -> dict[str, Any]:
+    """
+    maps field (string) to value, removes order
+    """
+    fields_json = get_fields_json(json_handler)
+    result = {}
+    for field_json in fields_json["fields"]:
+        result[field_json["name"]] = field_json
+    return result
 
 if __name__ == "__main__":
     json_handler = JsonHandler(True, True)
